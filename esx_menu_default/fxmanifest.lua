39f9cf7b8523c9c1551eee3f2edd48dfb4823f84
HypraWasTaken <104398254+HypraWasTaken@users.noreply.github.com>
2022-06-02 17:53:19 +0100

    HYPRA

---
+++ esx_menu_default/fxmanifest.lua
@@ -0,0 +1,31 @@
+
+
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'HYPRA MENU'
+
+version '1.0.4'
+
+client_scripts {
+	'@es_extended/client/wrapper.lua',
+	'client/main.lua'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'html/ui.html',
+	'html/css/app.css',
+	'html/js/mustache.min.js',
+	'html/js/app.js',
+	'html/fonts/LEMONMILK-Bold.otf',
+	'html/fonts/*.ttf'
+}
+
+dependencies {
+	'es_extended'
+}
